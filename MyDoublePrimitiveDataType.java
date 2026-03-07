18a6374bbf914337898dab2d3b58111a9b120ae2
rupanshuraman <220330189+omanshu-infoneo@users.noreply.github.com>
2026-03-07 21:45:36 +0530

    Practice double primitive data type

---
+++ MyDoublePrimitiveDataType.java
@@ -0,0 +1,11 @@
+public class Exercise {
+    
+    public static void main (String[] args) {
+        
+                /* Write your code below */
+        double piValue = 3.14;
+        System.out.println(piValue);
+        
+    }
+
+}
