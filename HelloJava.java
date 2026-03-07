fdd130f4896ea8a80d296f84bb005c399ba5f99c
rupanshuraman <220330189+omanshu-infoneo@users.noreply.github.com>
2026-03-07 21:40:01 +0530

    Print Hello Java

---
+++ HelloJava.java
@@ -0,0 +1,11 @@
+public class Exercise {
+    
+     public static void main (String[] args) {
+ 
+        /* Write your Java Statement below */
+	System.out.println("Hello Java");
+ 
+     }
+
+
+}
