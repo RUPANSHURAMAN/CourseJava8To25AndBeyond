27ccae1807286a8c9bedbaa98b11d7cb1cf4f87d
rupanshuraman <220330189+omanshu-infoneo@users.noreply.github.com>
2026-03-07 21:47:30 +0530

    Practice boolean primitive data type

---
+++ MyBooleanPrimitiveDataType.java
@@ -0,0 +1,12 @@
+    public static void main (String[] args) {
+        
+        /* Write your code below */
+        boolean believesInAliens = false, needsMoreCoffee = true;
+        
+        System.out.println(believesInAliens);
+        System.out.println(needsMoreCoffee);
+        
+        
+    }
+
+}
