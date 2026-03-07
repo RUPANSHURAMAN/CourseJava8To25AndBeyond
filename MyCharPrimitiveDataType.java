2f179f99441f419e70ae259eac57a6b3f28e2f73
rupanshuraman <220330189+omanshu-infoneo@users.noreply.github.com>
2026-03-07 21:57:14 +0530

    Practice char primitive data type

---
+++ MyCharPrimitiveDataType.java
@@ -0,0 +1,11 @@
+public class Exercise {
+    
+    public static void main (String[] args) {
+        
+        /* Write your code below */
+        char dollarSign = '$';
+        System.out.println(dollarSign);
+        
+    }
+
+}
