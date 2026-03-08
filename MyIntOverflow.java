3fd0bf6fa9560317812f7af788a15d55edf3b7bf
rupanshuraman <220330189+omanshu-infoneo@users.noreply.github.com>
2026-03-08 11:18:59 +0530

    Practive overflow of int data type

---
+++ MyIntOverflow.java
@@ -0,0 +1,11 @@
+public class Exercise {
+    
+    public static void main (String[] args) {
+        
+        /* Write your code below */
+        int overflowNum = Integer.MAX_VALUE + 1;
+        System.out.println(overflowNum);
+        
+    }
+
+}
