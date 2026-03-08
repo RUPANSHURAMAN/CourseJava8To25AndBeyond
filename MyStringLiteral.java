2999a3d9a5888d9782ed807b0344cc3702ab0dc0
rupanshuraman <220330189+omanshu-infoneo@users.noreply.github.com>
2026-03-08 16:23:37 +0530

    Practice String reference data type

---
+++ MyStringLiteral.java
@@ -0,0 +1,11 @@
+public class Exercise {
+    
+    public static void main (String[] args) {
+        
+        /* Write your code below */
+        String jokeOfTheDay = "Why was the computer cold? Because it left its Windows open!";
+        System.out.println(jokeOfTheDay);
+        
+    }
+
+}
