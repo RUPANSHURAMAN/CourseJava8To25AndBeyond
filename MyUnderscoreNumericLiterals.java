1851cb0112beb97cb7743633b37a520fa84eccc0
rupanshuraman <220330189+omanshu-infoneo@users.noreply.github.com>
2026-03-08 13:20:52 +0530

    Practice Numerical literals with underscores introduced in Java 7

---
+++ MyUnderscoreNumericLiterals.java
@@ -0,0 +1,11 @@
+public class Exercise {
+    
+    public static void main (String[] args) {
+        
+        /* Write your code below */
+        float piValue = 3.14_15_927f;
+        System.out.println(piValue);
+        
+    }
+
+}
