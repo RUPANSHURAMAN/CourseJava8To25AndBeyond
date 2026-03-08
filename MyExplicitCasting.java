1fd2a6d4f0f97f04614c7796d37d2f925c213d9f
rupanshuraman <220330189+omanshu-infoneo@users.noreply.github.com>
2026-03-08 16:09:40 +0530

    Practice Explicit casting float to int

---
+++ MyExplicitCasting.java
@@ -0,0 +1,12 @@
+public class Exercise {
+    
+    public static void main (String[] args) {
+        
+        /* Write your code below */
+        float measurement = 4.75F;
+        int recordedDepth = (int) measurement;
+        System.out.println(recordedDepth);
+        
+    }
+
+}
