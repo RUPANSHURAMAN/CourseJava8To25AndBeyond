a8aa1451d6eeab33f2828663f3d3d5e8bd47ac36
rupanshuraman <26063397+RUPANSHURAMAN@users.noreply.github.com>
2026-03-18 09:48:43 +0530

    Practice static main method

---
+++ StaticMainExercise.java
@@ -0,0 +1,7 @@
+class JokeProcessor{
+    static String hilariousJoke = "Why don't programmers like nature? It has too many bugs!";
+    
+    public static void main (String[] args) {
+        System.out.println(hilariousJoke);
+    }
+}
