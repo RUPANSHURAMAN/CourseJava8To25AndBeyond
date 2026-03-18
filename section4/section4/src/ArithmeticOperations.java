48f688ab353a78494d2d94d452122420d6a2459a
rupanshuraman <26063397+RUPANSHURAMAN@users.noreply.github.com>
2026-03-18 10:26:33 +0530

    Practice different method signatures

--- section4/section4/src/ArithmeticOperations.java
+++ section4/section4/src/ArithmeticOperations.java
@@ -2,4 +2,8 @@ public class ArithmeticOperations {
     public int sum(int num1, int num2){
         return num1 + num2;
     }
+
+     long sum(byte num3, int num2){
+        return num3 + num2;
+    }
 }
