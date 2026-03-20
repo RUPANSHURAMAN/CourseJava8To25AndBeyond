929a97d85b809a5b3bd60535a4eb7d6ff9b6d471
rupanshuraman <26063397+RUPANSHURAMAN@users.noreply.github.com>
2026-03-20 11:53:31 +0530

    Practice multiple main methods and without their modifiers

---
+++ section4/section4/src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args, int button) {
+        System.out.println("Hello");
+    }
+
+    public  double main(){
+        return 0;
+    }
+}
