401e5d20ce6f3c874ef2824e0101e7de05f4c669
rupanshuraman <26063397+RUPANSHURAMAN@users.noreply.github.com>
2026-03-21 22:11:46 +0530

    Practice object creation and getters

---
+++ section4/section4/src/EmployeeMain.java
@@ -0,0 +1,12 @@
+public class EmployeeMain {
+    static void main(String[] args) {
+        Employee employee = new Employee();
+        employee.firstName = "John";
+        employee.lastName = "Doe";
+        employee.age = 16;
+        employee.gender = 'M';
+
+        System.out.println(employee.getAge());
+        System.out.println(employee.getGender());
+    }
+}
