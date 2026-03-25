6e40437ba100ea540c9371fcfb32b70256161595
rupanshuraman <26063397+RUPANSHURAMAN@users.noreply.github.com>
2026-03-25 20:00:02 +0530

    Access static and instance methods from static and non static contexts

--- section4/section4/src/ArithmeticOperations.java
+++ section4/section4/src/ArithmeticOperations.java
@@ -1,9 +1,31 @@
 public class ArithmeticOperations {
+
+    public double  piValue = 3.14;
+    static final double PI_VALUE = 3.14;
+
+    static void main(String[] args) {
+        ArithmeticOperations  arithmeticOperations = new ArithmeticOperations();
+        int result = arithmeticOperations.sum(20,5);
+        System.out.println(result);
+        System.out.println(PI_VALUE);
+        System.out.println(arithmeticOperations.piValue );
+    }
+
     public int sum(int num1, int num2){
+        System.out.println(PI_VALUE);
+        System.out.println(piValue);
         return num1 + num2;
     }
 
+    public int subtract(int num1, int num2){
+        return num1 - num2;
+    }
+
      long sum(byte num3, int num2){
+        return num3 + num2;
+    }
+
+    public static int addition(int num3, int num2){
         return num3 + num2;
     }
 }
