6e23d4baaa2629e7273963cc4c88a72ffdf712d3
rupanshuraman <26063397+RUPANSHURAMAN@users.noreply.github.com>
2026-03-31 12:26:35 +0530

    Practive single line comment to describe above or end or comment entire line of code

---
+++ section5/src/SingleLineCommmentDemo.java
@@ -0,0 +1,24 @@
+public class SingleLineCommmentDemo {
+
+    int age; // Represents the age of customer
+
+    // Maximum number of login retry attempts allowed
+    final static int  MAX_RETRY_ATTEMPTS = 3;
+
+    static void main(String[] args) {
+
+        // N indicates NO
+        char n = 'N';
+
+        // Y indicates YES
+        char y = 'Y';
+
+//        sum(12,8);
+    }
+
+    public static void sum(int num1, int num2){
+        int num3 = num1 + num2;
+        System.out.println(num3);
+    }
+
+}
