da4172f98599195cfe413c6110d1127d4fa70b7e
rupanshuraman <26063397+RUPANSHURAMAN@users.noreply.github.com>
2026-04-02 11:44:08 +0530

    Use and generate javadoc html and comments

---
+++ section5/src/JavaDocCommentsDemo.java
@@ -0,0 +1,33 @@
+/**
+ * This is a sample class to show demo about
+ * javadoc
+ *
+ * <p>
+ *     This is a <b>Paragrabh</b>
+ * </p>
+ *
+ * @see MultiLineCommentsDemo
+ *
+ * @author  Rupanshu Raman
+ * @since  1.0
+ */
+public class JavaDocCommentsDemo {
+
+    static void main(String[] args) {
+
+    }
+
+    /**
+     * Take two {@code int} numbers as input
+     *
+     * <p>
+     *     <b>For example 2+3=5</b>
+     * </p>
+     * @param num1 Represents first number
+     * @param num2 Represents second number
+     * @return sum of first and second number
+     */
+    public int sum(int num1, int num2){
+    return num1 + num2;
+    }
+}
