87496e57199918169d3f1611f870926d75a71e17
rupanshuraman <26063397+RUPANSHURAMAN@users.noreply.github.com>
2026-04-02 10:55:57 +0530

    Use multiline comments

---
+++ section5/src/Customer.java
@@ -0,0 +1,11 @@
+/*
+  This class represents a customer in Bank app.
+  It contains information such as firstname,
+  lastname, DOB, address and account details.
+ */
+public class Customer {
+
+    static void main(String[] args) {
+
+    }
+}
