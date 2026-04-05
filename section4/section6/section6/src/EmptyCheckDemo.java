b2f044780a34a155f8fc7586bc19c9f55f2139fd
rupanshuraman <26063397+RUPANSHURAMAN@users.noreply.github.com>
2026-04-05 20:34:10 +0530

    Use empty method

--- section4/section6/section6/src/EmptyCheckDemo.java
+++ section4/section6/section6/src/EmptyCheckDemo.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class EmptyCheckDemo {
+    static void main(String[] args) {
+        String myString = "";
+        boolean isEmpty = myString.isEmpty();
+
+        boolean isLenghtZero = myString.length()==0;
+
+        boolean isEqualEmpty = "".equals(myString);
+
+        String nullString = null;
+        boolean isnullEmpty = "".equals(nullString);
+
+        boolean isEmptyNull = nullString.length()==0;
+
+        String blankString = "     ";
+        boolean isBlankEmpty = blankString.isEmpty();
+        boolean isBlank = blankString.isBlank();
+    }
 }
