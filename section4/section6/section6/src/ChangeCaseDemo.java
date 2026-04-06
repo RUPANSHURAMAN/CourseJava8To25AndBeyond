14a17ea8f46b7fe3d858ed41aa5dd0e8ffa1769d
rupanshuraman <26063397+RUPANSHURAMAN@users.noreply.github.com>
2026-04-06 20:45:05 +0530

    Use toUpperCase and toLowerCase method of String

---
+++ section4/section6/section6/src/ChangeCaseDemo.java
@@ -0,0 +1,14 @@
+import java.util.Locale;
+
+public class ChangeCaseDemo {
+    static void main(String[] args) {
+        String originalString = "Java";
+        String upperCaseString = originalString.toUpperCase();
+        String lowerCaseString = originalString.toLowerCase();
+
+        String specialString = "Java1@Best";
+        String specialUpperCaseString = specialString.toUpperCase();
+        String specialLowerCaseString = specialString.toLowerCase();
+
+    }
+}
