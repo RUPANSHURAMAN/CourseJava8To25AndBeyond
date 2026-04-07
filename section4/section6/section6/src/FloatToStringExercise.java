13ec4eb8281d48649784f6bd9210e28f8cc0403b
rupanshuraman <26063397+RUPANSHURAMAN@users.noreply.github.com>
2026-04-07 20:07:38 +0530

    Exercise valueOf method of String class

---
+++ section4/section6/section6/src/FloatToStringExercise.java
@@ -0,0 +1,13 @@
+class FloatToString {
+
+    public static void main (String[] args) {
+
+        float price = 9.99f;
+
+        /* Write your code below */
+        System.out.println(String.valueOf(price));
+
+
+    }
+
+}
