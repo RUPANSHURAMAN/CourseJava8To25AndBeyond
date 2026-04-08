799aab27e48b651eea10032b5b68df3b3df1543c
rupanshuraman <26063397+RUPANSHURAMAN@users.noreply.github.com>
2026-04-08 21:46:37 +0530

    Exercise contains method in String class

---
+++ section4/section6/section6/src/EmojiDetector.java
@@ -0,0 +1,12 @@
+public class EmojiDetector {
+
+    public static void main (String[] args) {
+
+        String message = "I love coding! 😊";
+
+        /* Write your code below */
+        System.out.println(message.contains("😊"));
+
+    }
+
+}
--- section4/section6/section6/src/StringSearchDemo.java
+++ section4/section6/section6/src/StringSearchDemo.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class StringSearchDemo {
+    static void main(String[] args) {
+        String originalString = "Hello, World!";
+        int index = originalString.indexOf('!');
+        int index1 = originalString.indexOf("World");
+
+        int index2 = originalString.indexOf("@");
+
+        boolean isContains = originalString.contains("World");
+        boolean isPresent = originalString.contains("@");
+
+        boolean startsWith = originalString.startsWith("Hello");
+        boolean endsWith = originalString.endsWith("@");
+
+        boolean isMatched = originalString.matches(".*World.*");
+    }
 }
