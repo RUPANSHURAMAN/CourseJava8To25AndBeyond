422f6ff63125986064af90ab180a6daf85add0cb
rupanshuraman <26063397+RUPANSHURAMAN@users.noreply.github.com>
2026-04-09 21:43:07 +0530

    Practice substring method in String class

---
+++ section4/section6/section6/src/SubStringExercise.java
@@ -0,0 +1,12 @@
+class SurpriseGift {
+
+    public static void main (String[] args) {
+
+        String gift = "Surprise! It's a puppy!";
+
+        /* Write your code below */
+        System.out.println(gift.substring(0,9));
+
+    }
+
+}
