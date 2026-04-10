578be49426b7bee67590d77f9c0e3a779242eb6d
rupanshuraman <26063397+RUPANSHURAMAN@users.noreply.github.com>
2026-04-10 20:11:05 +0530

    Practice replace, replaceAll and replaceFirst

---
+++ section4/section6/section6/src/StringReplaceExercise.java
@@ -0,0 +1,11 @@
+class CookingRecipe {
+
+    public static void main (String[] args) {
+
+        String recipe = "Add salt and pepper to taste.";
+
+        // Write your code below
+        System.out.println(recipe.replace("pepper", "cinnamon"));
+    }
+
+}
